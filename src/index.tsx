62ff336c74e0df993b989786df5671f1e5572962
simochee <lollipop@simochee.net>
2025-02-27 02:38:10 +0900

    chore: prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <p>Hello World</p>
-  </StrictMode>
-)
+createRoot(document.getElementById("root") as HTMLElement).render(
+	<StrictMode>
+		<p>Hello World</p>
+	</StrictMode>,
+);
